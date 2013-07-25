a50420455f27da503af03c03865ab310cdcbfbdd
elguntor <elguntor@gmail.com>
2013-07-24 22:40:32 -0400

    Revert "Removed emacs.d"
    This reverts commit 61e32e0e8e1ef095124346ffcdc053b519e50006.

---
+++ .emacs.d/init.el
@@ -0,0 +1,7 @@
+;;; package -- init.el
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" .
+               "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+(add-hook 'after-init-hook #'global-flycheck-mode)
