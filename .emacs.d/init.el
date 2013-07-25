61e32e0e8e1ef095124346ffcdc053b519e50006
elguntor <elguntor@gmail.com>
2013-07-24 22:13:27 -0400

    Removed emacs.d

--- .emacs.d/init.el
+++
@@ -1,7 +0,0 @@
-;;; package -- init.el
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa" .
-               "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
-(add-hook 'after-init-hook #'global-flycheck-mode)
